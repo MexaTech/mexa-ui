35bfadc8ee665e96cf9705390627b72334120cb8
edsonfervo16mx <edson55_@hotmail.com>
2023-02-04 23:54:35 -0500

    Cambio de estructura de libreria, test 01

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,24 +1,28 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
-import { join } from "pathe";
-import Components from "unplugin-vue-components/vite";
+// import { join } from "pathe";
+// import Components from "unplugin-vue-components/vite";
 
-export default defineNuxtConfig({
-  css: [
-    "primevue/resources/themes/saga-blue/theme.css",
-    "primevue/resources/primevue.css",
-    "primeicons/primeicons.css",
-  ],
-  build: {
-    transpile: ["primevue"],
-  },
-  //   vite: {
-  //     plugins: [
-  //       Components({
-  //         extensions: ["vue"],
-  //         include: [/\.vue$/, /\.vue\?vue/],
-  //         dts: "src/components.d.ts",
-  //         dirs: ["src/**/*", "node_modules/primevue/**/*"],
-  //       }),
-  //     ],
-  //   },
-});
+// export default defineNuxtConfig({
+// css: [
+//   "primevue/resources/themes/saga-blue/theme.css",
+//   "primevue/resources/primevue.css",
+//   "primeicons/primeicons.css",
+// ],
+// build: {
+//   transpile: ["primevue"],
+// },
+//   vite: {
+//     plugins: [
+//       Components({
+//         extensions: ["vue"],
+//         include: [/\.vue$/, /\.vue\?vue/],
+//         dts: "src/components.d.ts",
+//         dirs: ["src/**/*", "node_modules/primevue/**/*"],
+//       }),
+//     ],
+//   },
+// });
+
+module.exports = {
+  plugins: [{ src: "~/plugins/index.js", mode: "client" }],
+};
--- plugins/primevue.js
+++ plugins/primevue.js
@@ -1,7 +1,7 @@
 import { defineNuxtPlugin } from "#app";
-import PrimeVue from "primevue/config";
-import Slider from "primevue/slider";
+// import PrimeVue from "primevue/config";
+// import Slider from "primevue/slider";
 
 export default defineNuxtPlugin((nuxtApp) => {
-  nuxtApp.vueApp.use(PrimeVue).component("Slider", Slider);
+  // nuxtApp.vueApp.use(PrimeVue).component("Slider", Slider);
 });
