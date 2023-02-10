c5bb6200e71072740c80c4b0135294f4a01f603f
edsonfervo16mx <edson55_@hotmail.com>
2023-02-10 10:23:14 -0500

    Image component, using props[sc-128]

--- pages/team-sections/basic.vue
+++ pages/team-sections/basic.vue
@@ -3,9 +3,16 @@
     Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugiat repudiandae
     asperiores adipisci dolor voluptatem ipsam ex saepe tenetur amet corrupti,
     ullam cum illo sapiente odio officiis voluptatibus placeat quibusdam dolore.
-    <image-basic />
+    <image-basic v-bind="properties" />
   </div>
 </template>
 <script setup>
 import ImageBasic from "~~/components/Images/ImageBasic.vue";
+const properties = ref({
+  src: "https://via.placeholder.com/150",
+  alt: "Image example",
+  width: "250",
+  preview: true,
+  imageClass: "",
+});
 </script>
