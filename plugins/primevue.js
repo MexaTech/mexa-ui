89ab211864c91e01bc293c854a88276928af3ff8
Rodolfo Brena <rod0brcz@gmail.com>
2023-02-18 21:05:34 -0600

    Profile [sc-125]

--- plugins/primevue.js
+++ plugins/primevue.js
@@ -4,12 +4,15 @@ import Slider from "primevue/slider";
 import Button from "primevue/button";
 import InputText from "primevue/inputtext";
 import Checkbox from "primevue/checkbox";
-
+import Dropdown from 'primevue/dropdown';
+import CascadeSelect from 'primevue/cascadeselect';
 export default defineNuxtPlugin((nuxtApp) => {
   nuxtApp.vueApp
     .use(PrimeVue)
     .component("Slider", Slider)
     .component("Button", Button)
     .component("InputText", InputText)
+    .component("Dropdown", Dropdown)
+    .component("CascadeSelect", CascadeSelect)
     .component("Checkbox", Checkbox);
 });
