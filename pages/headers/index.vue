7bc6ad678a983102bde5aae7595afcf7641c421e
edsonfervo16mx <edson55_@hotmail.com>
2023-02-21 11:29:01 -0500

    Call to Action, component props [sc-127]

--- pages/headers/index.vue
+++ pages/headers/index.vue
@@ -1,6 +1,8 @@
 <template>
   <mexa-call-to-action />
+  <mexa-banner />
 </template>
 <script setup>
 import MexaCallToAction from "@/components/Headers/MexaCallToAction.vue";
+import MexaBanner from "@/components/Headers/MexaBanner.vue";
 </script>
